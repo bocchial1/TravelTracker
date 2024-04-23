facce368d128152b03c325ed710181f5f35dbc5c
Alex Bocchi <bocchial@msu.edu>
2024-04-23 14:02:02 -0500

    updates to web-app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -11,4 +12,14 @@ import { HomeComponent } from './home/home.component';
 })
 export class AppComponent {
   title = 'my-app';
+
+  // const config: sql.config = {
+  //   server: 'travel-tracker-server.database.windows.net',
+  //   database: 'TravelTracker',
+  //   user: 'bocchial',
+  //   password: 'Zakumi01@',
+  //   options: {
+  //     encrypt: true, // Enable encryption if needed
+  //   },
+  // };
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent{
+  progress: number = 50; // Set initial progress to 50%
+  steps: any[] = [
+    { label: 'Step 1', width: 20 },
+    { label: 'Step 2', width: 30 },
+    { label: 'Step 3', width: 25 },
+    { label: 'Step 4', width: 25 }
+  ];
 
+  constructor() { }
 }
